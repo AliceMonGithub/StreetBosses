0f49f851f4444b7242ba4e40ae5ce9ca58ef4e72
Alice Mon <88655866+AliceMonGithub@users.noreply.github.com>
2023-09-27 13:31:15 +0300

    darkness-and-parallax

--- Assets/Sources/Client/Menues/PopupMenu.cs
+++ Assets/Sources/Client/Menues/PopupMenu.cs
@@ -11,27 +11,37 @@ namespace Client.MenuesLogic
 
         [Space]
 
+        [SerializeField] private GameObject _bgGameObject;
+        [SerializeField] private CanvasGroup _bgCanvasGroup;
+
+        [Space]
+
         [SerializeField] private float _showDuration;
 
         public void Show()
         {
             _transform.localScale = Vector3.zero;
             _canvasGroup.alpha = 0f;
+            _bgCanvasGroup.alpha = 0f;
 
             _root.SetActive(true);
+            _bgGameObject.SetActive(true);
 
             _transform.DOScale(Vector3.one, _showDuration);
+            _bgCanvasGroup.DOFade(1, _showDuration);
             _canvasGroup.DOFade(1, _showDuration);
         }
 
         protected void Hide()
         {
             _transform.DOScale(Vector3.zero, _showDuration);
+            _bgCanvasGroup.DOFade(0, _showDuration);
             _canvasGroup.DOFade(0, _showDuration).onComplete += Deactivate;
         }
 
         private void Deactivate()
         {
+            _bgGameObject.SetActive(false);
             _root.SetActive(false);
         }
     }
